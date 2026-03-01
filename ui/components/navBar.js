27a71fb5125e25309636935bc23eecdd1b5e4573
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-02 06:40:15 +0700

    ttzz

--- ui/components/navBar.js
+++ ui/components/navBar.js
@@ -1,24 +1,17 @@
 // ui/components/navBar.js
 
-import { UI } from "../../core/state.js";
-import { translate } from "../utils/translate.js";
 import { initLangSwitch } from "../langController.js";
-import { getContext } from "../core/context.js";
-import { PLACES } from "../data/places.js";
-import { t } from "../data/i18n.js";
+import { renderContextBar } from "../ui/renderContextBar.js";
 
 export function renderNavBar(){
 
   const el = document.getElementById("contextBar");
-  if(!el) return;
-
-    const isHome = UI.view.screen==="home";
 
   
 el.innerHTML = `
   <div class="nav">
     <div class="nav-bar nav-left"> Duc Ninh Haven </div>
-    <div class="nav-bar nav-center">${getContext()}</div>
+    <div class="nav-bar nav-center"></div>
     <div class="nav-bar nav-right">
       <div id="langSwitch" class="lang-switch">
         <button data-lang="vi">VI</button> <button data-lang="en">EN</button>
@@ -32,30 +25,3 @@ el.innerHTML = `
 
 /* ---------- helpers ---------- */
 
-
-
-function renderContextBar(){
-
-  const el=document.querySelector(".nav-center");
-  if(!el) return;
-
-  const ctx=getContext();
-
-  if(!ctx){
-    el.innerHTML=`<span class="no-context">${t("choose_place")}</span>`;
-    return;
-  }
-
-  const label = PLACES[ctx.type+"s"][ctx.id].label;
-
-  el.innerHTML=`
-    <span class="ctx-icon">${icon(ctx.type)}</span>
-    <span class="ctx-label">${t(label)}</span>
-  `;
-}
-
-function icon(type){
-  if(type==="room") return "🛏";
-  if(type==="table") return "🍽";
-  return "📍";
-}
