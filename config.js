90b9708f2ba2bd108c405f5d5e7c9c4790f2ef6a
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-02 15:17:15 +0700

    qq

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export const CONFIG = {
   API_ENDPOINT: "https://script.google.com/macros/s/AKfycbz4WVqouGUtfLOYHEqIT0oNmEMEWXUmGeR514_uk1Gbi_DnDi4vXpgNUq49nqIehwtM/exec",
   API_SECRET: "HNV-2026",
 
-  VERSION: "1.0.1"
+  VERSION: "1.0.2"
 
 };
