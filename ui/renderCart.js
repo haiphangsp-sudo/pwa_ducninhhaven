2ff5ca7cb0adaf11e11ecee7fa4882ade1b2a1ac
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-05 11:41:36 +0700

    yh

--- ui/renderCart.js
+++ ui/renderCart.js
@@ -9,17 +9,26 @@ import { translate } from "./utils/translate.js";
 export function renderCartBar(){
 
   const bar = document.getElementById("cartBar");
-  if(!bar) return;
+  const count = document.getElementById("cartCount");
+  const items = UI.cart.items;
+  const total = items.reduce((a,b)=>a+b.qty,0);
 
-  const count = UI.cart.items.reduce((a,b)=>a+b.qty,0);
+  //const count = UI.cart.items.reduce((a,b)=>a+b.qty,0);
 
   const ctx = getContext();
 
-  const countEl = document.getElementById("cartCount");
+  //const countEl = document.getElementById("cartCount");
   const sendBtn = document.getElementById("cartSend");
+  sendBtn.disabled = total===0;
   let textOrder = "select_place";
-  countEl.textContent = count;
-
+  //countEl.textContent = count;
+  if(total==0){
+    bar.classList.add("hidden");
+    return;
+  }else{
+    bar.classList.remove("hidden");
+  }
+  
   if(!ctx){
     textOrder="select_place";
     sendBtn.onclick = ()=>window.dispatchEvent(new Event("openPlacePicker"));
@@ -30,7 +39,8 @@ export function renderCartBar(){
     if(UI.delivery.state==="sending"){
       textOrder="delivery.pending";
     }
-    textOrder="send_order";
+    textOrder="cart_bar.send_order";
   }
+  count.textContent = `${total} ${translate("cart_bar.items")}`;
   sendBtn.textContent=translate(textOrder);
 }
