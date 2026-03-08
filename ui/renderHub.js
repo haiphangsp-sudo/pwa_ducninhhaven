72e9c37d6edf0a48f85806065d4e2def049e41f8
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-08 10:19:48 +0700

    yu

--- ui/renderHub.js
+++ ui/renderHub.js
@@ -49,7 +49,7 @@ export function renderHub(){
   menuEl.innerHTML = panels.map(key=>`
     <button class="hub-btn ${panel===key?"active":""}"
             data-key="${key}"
-            data-type=${MENU.key.type}>
+            data-type=${cat.type}>
       <span class="hub-icon">${ICONS[key] || ""}</span>
       <span class="hub-label">${translate(MENU[key].label)}</span>
     </button>
