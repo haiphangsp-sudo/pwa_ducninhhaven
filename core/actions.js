ab3eb7bb3703d5c71d6b162322e78da53a7f1632
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-08 10:59:10 +0700

    io

--- core/actions.js
+++ core/actions.js
@@ -48,7 +48,7 @@ export function sendInstant(action){
   setState({ack:{state:"show"}});
 
   enqueue({
-    actionType: action.type,
+    type: action.type,
     place: ctx.active.id,
     placeType: ctx.active.type,
     category: action.category,
@@ -70,7 +70,6 @@ export function sendCart(){
   }
 
   enqueue({
-    actionType:"cart",
     place: ctx.active.id,
     placeType: ctx.active.type,
     items: structuredClone(UI.cart.items)
