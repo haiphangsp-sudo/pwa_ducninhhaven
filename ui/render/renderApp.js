d171fd5cd430a8c3fa2e65d76b6f2a1ffbccb5c1
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-11 15:25:38 +0700

    hk

--- ui/render/renderApp.js
+++ ui/render/renderApp.js
@@ -1,12 +1,12 @@
 // ui/renderApp.js
 // Điểm vào duy nhất để render.
 
-import { renderCartBar ,loadCart} from ".renderCart.js";
+import { renderCartBar ,loadCart} from "./renderCart.js";
 import { renderAck } from "./renderOverlay.js";
 import { setDeliveryState } from "./renderDelivery.js";
 import { setRecoveryState } from "./renderRecovery.js";
-import { initLangSwitch } from "./utils/translate.js";
-import { renderNavBar } from "./components/navBar.js";
+import { initLangSwitch } from "../utils/translate.js";
+import { renderNavBar } from "../components/navBar.js";
 import { renderHub } from "./renderHub.js";
 
 export function renderApp(){
