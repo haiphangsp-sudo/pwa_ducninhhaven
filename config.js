8862da60661d9ace5ecc3fe0764f66f42331975a
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-17 23:31:53 +0700

    r

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ export const CONFIG = {
 
   API_ENDPOINT: "https://script.google.com/macros/s/AKfycbzWlIDLcPwwV9k8xHQEEPNkWOi1P06D52OwA9I9jB6BiSfejeYWsz7R_ilCMgTbOK8X/exec",
   API_SECRET: "HNV-2026",
-  VERSION: "1.3.2" // Cập nhật version để SW nhận biết và refresh cache
+  VERSION: "2.0.0" // Cập nhật version để SW nhận biết và refresh cache
 };
