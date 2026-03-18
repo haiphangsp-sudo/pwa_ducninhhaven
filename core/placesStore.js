6d72af6bfa86dd55bff0178f5d3c9ea65a4ea6ec
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-18 20:23:35 +0700

    c

--- core/placesStore.js
+++ core/placesStore.js
@@ -3,8 +3,7 @@ export let PLACES = {};
 export let PLACE_UI = {};
 
 export async function loadPlaces() {
-    const res = await fetch("/data/places.json", { cache: "no-cache" }).then(res => res.json());
-    const data = await res.json();
+    const data = await fetch("/data/places.json", { cache: "no-cache" }).then(res => res.json());
     PLACES = data.places || {};
     PLACE_UI = data.ui || {};
 };
