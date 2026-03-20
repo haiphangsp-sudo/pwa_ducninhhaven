74acbdf1d263cda69b7ef523cebf73f9686029b7
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-20 08:22:11 +0700

    dd

--- ui/render/renderApp.js
+++ ui/render/renderApp.js
@@ -5,14 +5,14 @@ import { renderCartBar } from "./renderCart.js";
 import { renderAck } from "./renderOverlay.js";
 import { renderNavBar } from "../components/navBar.js";
 import { renderHub } from "./renderHub.js";
+import { renderDrawer } from "./renderDrawer.js";
 
 
 export function renderApp(){
-  
   renderNavBar();
   renderHub();
   renderCartBar()
   renderAck();
-
+  renderDrawer();
 }
 
