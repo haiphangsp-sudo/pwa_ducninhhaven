ade2d9c58489884e98593e73367d95c0557491cf
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-20 22:14:04 +0700

    d

--- ui/render/renderDrawer.js
+++ ui/render/renderDrawer.js
@@ -6,7 +6,7 @@ import { closeOverlay, showOverlay } from "../interactions/backdropManager.js";
 import { MENU } from "../../core/menuStore.js";
 import { getCartTotals, textItemItems } from "../utils/cartCalculators.js";
 import { updateCartBarTotal } from "./renderCart.js";
-import { openPicker } from "./renderPlacePicker.js";
+import { openPicker } from "../components/placePicker";
 
 // Chúng ta chỉ cần lưu "Ảnh chụp lúc mở" để so sánh
 let initialCartSnapshot = ""; 
