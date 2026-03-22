d097f5b60c9177e8eccea245ed30ca8c37a767e8
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-22 16:43:01 +0700

    o

--- ui/render/renderDrawer.js
+++ ui/render/renderDrawer.js
@@ -117,7 +117,7 @@ export function attachDrawerEvents() {
       
       const action = sendBtn.dataset.action;
       if (action === "close") {
-        closeOverlay;
+        closeOverlay();
         return;
       }
 
