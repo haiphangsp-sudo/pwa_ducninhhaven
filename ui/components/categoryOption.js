ac1d285b76df02ce7c73a6a0d98ffd57d4fd4f02
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-25 01:02:06 +0700

    s

--- ui/components/categoryOption.js
+++ ui/components/categoryOption.js
@@ -32,7 +32,7 @@ export function categoryOpt(opt, itemKey, categoryKey, type) {
                     data-category="${categoryKey}"
                     data-item="${itemKey}"
                     data-value="${opt.key}"
-                    data-price="${price}
+                    data-price="${price}"
                     data-currency="vnd">
                     ${type==="cart" ? `+ ` + translate("cart_bar.add_to_order") : `⚡ ` + translate("cart_bar.send_request")}
                 </button>
