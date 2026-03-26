5c46a74cf95df9f1a55307604193796aaf31aecc
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-26 10:51:11 +0700

    d

--- ui/render/renderPanel.js
+++ ui/render/renderPanel.js
@@ -29,12 +29,13 @@ export function renderPanel(state) {
     }
   }
   const panels = document.getSelection(".category-panel");
-  panels.forEach(panel => {
-    if(panel.id !== nextPanel)
+  if (panels) {
+    panels.forEach(panel => {
+      if (panel.id !== nextPanel)
         panel.classList.add("hidden");
-    else
+      else
         panel.classList.remove("hidden");
-  
-  });
+    });
+  }
 }
 
