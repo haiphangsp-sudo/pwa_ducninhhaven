d3eee93a071fe75427e7298f1e9ba30097616bd9
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-28 12:05:15 +0700

    s

--- ui/render/renderArticle.js
+++ ui/render/renderArticle.js
@@ -2,7 +2,7 @@ import { translate } from "../utils/translate.js";
 
 export function renderArticle(articles){
   if (!articles) return "";
-  return articles.products.map(art => `
+  return articles.article.map(art => `
     <div class="article">
       <h2 class="article-title">${translate(art.label)}</h2> 
       <div class="article-content">
