24322dff1ffc8aaa4e3f344bf7dd0a80636de21b
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-29 15:02:42 +0700

    s

--- core/menuQuery.js
+++ core/menuQuery.js
@@ -80,7 +80,7 @@ export function getVariantById(id) {
             categoryLabel: translate(category.label),
             productLabel: translate(product.label),
             variantLabel: translate(variant.label),
-            price:`${Number(variant.price).toLocaleString("vi-VN")} đ`,
+            price: new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(variant.price),
             unit: variant.unit || "item",
             active: variant.active !== false,
             ui: category.ui || "cart"
