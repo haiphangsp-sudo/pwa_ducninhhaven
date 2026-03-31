81a7cff7592e88c4008ee8bf26a509da8dd9cb3b
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-03-31 15:08:54 +0700

    s

--- ui/render/renderOverlay.js
+++ ui/render/renderOverlay.js
@@ -1,6 +1,8 @@
 // ui/render/renderOverlay.js
 
 import { translate } from "../utils/translate.js";
+
+
 export function renderAck({ visible, status, message }) {
   const el = document.getElementById("ackOverlay");
   if (!el) return;
