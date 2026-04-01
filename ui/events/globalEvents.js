6eeb683d4f1d890517b7ae0cc4159bf3d6c98782
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-01 19:42:53 +0700

    s

--- ui/events/globalEvents.js
+++ ui/events/globalEvents.js
@@ -11,7 +11,7 @@ import { toggleStatusBar } from "../../ui/components/statusBar.js";
 
 document.querySelectorAll('.btn-add').forEach(btn => {
     btn.addEventListener('click', (e) => {
-        addToCart(e); // Gọi hàm bình thường vì cùng nằm trong module
+        addToCart(Event.target.dataset.id); // Gọi hàm bình thường vì cùng nằm trong module
     });
 });
 
