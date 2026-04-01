e880f3f03ebcf9b938e32397573f5cad372c9569
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-01 10:56:32 +0700

    s

--- ui/render/renderHub.js
+++ ui/render/renderHub.js
@@ -3,13 +3,14 @@
 
 import { translate } from "../utils/translate.js";
 import { getCategories } from "../../core/menuQuery.js";
-import { getCategoriesForMode } from "../../data/helpers.js";
+//import { getCategoriesForMode } from "../../data/helpers.js";
 
 export function renderHub(state) {
   const panels = getCategories();
   const menuEl = document.getElementById("hubMenu");
   if (!menuEl) return;
   const currentPanel = state.panel.view;
+  hubEvents(currentPanel);
   menuEl.innerHTML = panels.map(cat => {
     const isActive = cat.key === currentPanel ? "is-active" : "";
     return `
@@ -25,4 +26,17 @@ export function renderHub(state) {
       </span>
     </button>
   `}).join("");
+}
+function hubEvents(viewNew) {
+
+  const menuEl = document.getElementById("hubMenu");
+  const viewOld = menuEl.querySelector(`.is-active`).getAttribute("data-value");
+  if (viewNew === viewOld) return;
+    
+    menuEl.querySelectorAll("button").forEach(btn => {  
+      btn.classList.remove("is-active");
+      if ((btn.getAttribute("data-value") !== viewNew)) {
+        btn.classList.add("is-active");
+      }
+    });
 }
