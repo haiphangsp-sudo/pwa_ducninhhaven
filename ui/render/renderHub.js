ee35d0c39676578a028a91b7a4a3bca7531aa6a9
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-04 16:45:18 +0700

    s

--- ui/render/renderHub.js
+++ ui/render/renderHub.js
@@ -6,7 +6,7 @@ import { getCategoriesForCurrentPlace } from "../../core/menuQuery.js";
 
 export function renderHub(state) {
   const categories = getCategoriesForCurrentPlace();
-  const menuEl = document.getElementById("hubMenu");
+  const menuEl = document.getElementById("hub-container");
   if (!menuEl) return;
   const currentPanel = state.panel.view;
   menuEl.innerHTML = categories.map(cat => {
@@ -26,8 +26,24 @@ export function renderHub(state) {
   `}).join("");
 }
 export function eventHub(state) {
-  const menuEl = document.getElementById("hubMenu");
+  const menuEl = document.getElementById("hub-container");
   menuEl.querySelectorAll("button").forEach(btn => {
     btn.classList.toggle("is-active", btn.getAttribute("data-value") === state.panel.view);
   });
-} 
+
+  const hub = document.getElementById('hub-container');
+  const wrapper = document.getElementById('hubMenu');
+
+  if (!hub || !wrapper) return;
+
+  hub.addEventListener('scroll', () => {
+      // Kiểm tra xem khoảng cách cuộn + chiều rộng màn hình có bằng chiều rộng thực tế của list không
+      const isEnd = hub.scrollLeft + hub.offsetWidth >= hub.scrollWidth - 10;
+
+      if (isEnd) {
+          wrapper.classList.add('is-at-end');
+      } else {
+          wrapper.classList.remove('is-at-end');
+      }
+  });
+}
