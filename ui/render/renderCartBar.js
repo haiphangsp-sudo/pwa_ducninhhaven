ce8d83a16b5fb03436ad2347b632ee2a1094d357
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-05 15:37:59 +0700

    s

--- ui/render/renderCartBar.js
+++ ui/render/renderCartBar.js
@@ -48,7 +48,7 @@ let lastScrollTop = 0;
 const threshold = 10; // Khoảng cách cuộn tối thiểu để kích hoạt (tránh rung lắc)
 
 export function initSmartHeader() {
-    const contextBar = document.getElementById('contextBar');
+    const contextBar = document.getElementById('cartBar');
     if (!contextBar) return;
 
     window.addEventListener('scroll', () => {
@@ -59,10 +59,10 @@ export function initSmartHeader() {
 
         if (scrollTop > lastScrollTop && scrollTop > 100) {
             // CUỘN XUỐNG: Ẩn thanh bar
-            contextBar.classList.add('context-bar--hidden');
+            contextBar.classList.add('cart-bar--hidden');
         } else {
             // CUỘN LÊN: Hiện thanh bar
-            contextBar.classList.remove('context-bar--hidden');
+            contextBar.classList.remove('cart-bar--hidden');
         }
 
         lastScrollTop = scrollTop;
