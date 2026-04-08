2fa0f419f8296f549fca912d2f0dc12d626dcf07
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-08 10:29:49 +0700

    s

--- ui/render/renderPanel.js
+++ ui/render/renderPanel.js
@@ -4,20 +4,9 @@ import { renderArticle } from "./renderArticle.js";
 export function renderPanel(state) {
   const panelId = state.panel.view;
   const ui = state.panel.option;
-  const container = document.querySelector(".page-container");
-
+  const container = document.querySelector(".category-panel");
   if (!container || !panelId) return;
-
-  let panelEl = document.getElementById(panelId);
-  if (!panelEl) {
-    container.insertAdjacentHTML(
-      "beforeend",
-      `<div id="${panelId}" class="category-panel hidden"></div>`
-    );
-    panelEl = document.getElementById(panelId);
-  }
-
-  panelEl.innerHTML =
+  container.innerHTML =
         ui === "article"
           ? renderArticle(panelId)
           : renderMenu(panelId, ui);
