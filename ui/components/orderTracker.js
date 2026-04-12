fc3f2cbda8c230fbf3f2bc76290e32a205459fe9
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-12 09:28:25 +0700

    d

--- ui/components/orderTracker.js
+++ ui/components/orderTracker.js
@@ -75,18 +75,14 @@ function renderOrderCard(order = {}, showStepper = true) {
   return `
     <article class="tracker-order ${!showStepper ? "is-history" : ""}">
       <div class="tracker-order__header">
-        <div class="tracker-order__title">
-          <span class="tracker-order__code">#${escapeHtml(shortId)}</span>
+          <div class="tracker-order__code">#${escapeHtml(shortId)}</div>
           <span class="tracker-order__status status-badge is-${status.toLowerCase()}">
             ${escapeHtml(status)}
           </span>
-        </div>
-
         <div class="tracker-order__meta">
           ${placeLabel ? `<span>${escapeHtml(placeLabel)}</span>` : ""}
            <span class="tracker-order__time">${translate("order.time")}: ${time}</span>
         </div>
-
       </div>
 
       <div class="tracker-order__content">
