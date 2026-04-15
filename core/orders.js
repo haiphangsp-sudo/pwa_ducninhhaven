4a9f26b1b148dea76ea013d7fc7cb1a2a0a1f6bd
haiphangsp-sudo <haiphan.gsp@gmail.com>
2026-04-16 04:13:21 +0700

    a

--- core/orders.js
+++ core/orders.js
@@ -207,6 +207,8 @@ export function addOrderToTracking(meta = {}) {
 }
 
 export async function syncOrdersWithServer() {
+  markSyncingAgedOrders();
+
   const ids = getSavedIds();
   if (!ids.length) return;
 
